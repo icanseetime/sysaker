452b87832cf653079456e35aa47b547ae5fd3eb4
Ida <imgjeits@stud.ntnu.no>
2021-09-27 19:54:08 +0200

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import './App.css'
 
+// Components
+import Header from './components/navigation/Header/Header'
+
 function App() {
-	return <div className="App"></div>
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	)
 }
 
 export default App
---
+++ frontend/src/components/navigation/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+	return (
+		<header>
+			<h1>Mønster</h1>
+		</header>
+	)
+}
