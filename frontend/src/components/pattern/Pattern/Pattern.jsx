50f930ce6eff876ad673067e3c83b6f2c067dfb1
Ida <imgjeits@stud.ntnu.no>
2021-10-05 15:19:21 +0200

    added pen/pearl/eraser functionality

--- frontend/src/components/pattern/Pattern/Pattern.jsx
+++ frontend/src/components/pattern/Pattern/Pattern.jsx
@@ -4,15 +4,18 @@ import './Pattern.css'
 // Components
 import SingleCell from '../SingleCell/SingleCell'
 
-export default function Pattern({ size }) {
+export default function Pattern({ size, mode, color }) {
 	return (
 		<div
 			className="Pattern"
 			style={{
-				gridTemplateColumns: `repeat(${size.columns}, auto)`
+				gridTemplateColumns: `repeat(${size.columns}, 1fr)`,
+				gridTemplateRows: `repeat(${size.rows}, 1fr)`
 			}}
 		>
-			{Array(size.columns * size.rows).fill(<SingleCell />)}
+			{Array(size.columns * size.rows).fill(
+				<SingleCell mode={mode} color={color} />
+			)}
 		</div>
 	)
 }
--- frontend/src/components/pattern/SingleCell/SingleCell.jsx
+++ frontend/src/components/pattern/SingleCell/SingleCell.jsx
@@ -1,15 +1,59 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './SingleCell.css'
+
+// Utils
 import { ThemeContext } from '../../../utils/ThemeContext'
 
-export default function SingleCell() {
+export default function SingleCell({ mode, color }) {
 	const { theme } = useContext(ThemeContext)
+	const [fill, setFill] = useState({
+		color: 'transparent',
+		pearl: false
+	})
+
+	const handleColorChange = (color) => {
+		setFill((prev) => {
+			return { ...prev, color }
+		})
+	}
+
+	const setPearl = (pearl) => {
+		setFill((prev) => {
+			return { ...prev, pearl }
+		})
+	}
+
+	const handleErase = () => {
+		setFill({
+			color: 'transparent',
+			pearl: false
+		})
+	}
+
 	return (
 		<span
 			className="SingleCell"
 			style={{
-				border: `1px solid ${theme.secondary}`
+				border: `1px solid ${theme.secondary}`,
+				backgroundColor: fill.color
 			}}
-		></span>
+			onClick={() => {
+				switch (mode) {
+					case 'pen':
+						handleColorChange(color)
+						break
+					case 'pearl':
+						setPearl(true)
+						break
+					case 'eraser':
+						handleErase()
+						break
+					default:
+						throw Error('No mode selected')
+				}
+			}}
+		>
+			{fill.pearl && <div className="pearl"></div>}
+		</span>
 	)
 }
